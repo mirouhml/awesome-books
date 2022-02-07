4209dd591023e1068955d0e5895ad672699faa83
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-02-07 16:14:09 +0100

    Add localStorage, add event listener for remove.

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
 let books = [
-    {
-        title: 'book1',
-        author: 'author1'
-    },
-    {
-        title: 'book2',
-        author: 'author2'
-    },
-]
+  {
+    title: 'book1',
+    author: 'author1',
+  },
+  {
+    title: 'book2',
+    author: 'author2',
+  },
+];
 
-function addBook(title,author) {
-    const book = {
-        title: title,
-        author: author
-    }
-    books.push(book);
+function populateStorage(booksList) {
+  localStorage.setItem('books', JSON.stringify(booksList));
+}
+
+function addBook(bookTitle, bookAuthor) {
+  const book = {
+    title: bookTitle,
+    author: bookAuthor,
+  };
+  books.push(book);
+  populateStorage(books);
 }
 
 function removeBook(title) {
-    books = books.filter(book => book.title !== title)
+  books = books.filter((book) => book.title !== title);
+  populateStorage(books);
 }
 
 function displayBooks() {
-    const booksContainer = document.getElementById('books-list');
-    for(let i=0; i<books.length; i+=1) {
-        const book = document.createElement('li');
-        book.innerHTML = `<h2>${books[i].title}</h2>
+  const booksContainer = document.getElementById('books-list');
+  if (localStorage.getItem('books')) {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  booksContainer.innerHTML = '';
+  for (let i = 0; i < books.length; i += 1) {
+    const book = document.createElement('li');
+    book.innerHTML = `<h2>${books[i].title}</h2>
                         <h3>${books[i].author}</h3>
-                        <button type="button">Remove</button>
+                        <button id="book${i}" type="button">Remove</button>
                         <hr>`;
-        booksContainer.appendChild(book);
-    }
+    booksContainer.appendChild(book);
+    const { title } = books[i];
+    document.getElementById(`book${i}`).addEventListener('click', () => {
+      removeBook(title);
+      displayBooks();
+    });
+  }
 }
 
 displayBooks();
@@ -38,6 +53,8 @@ displayBooks();
 const button = document.getElementById('add');
 
 button.addEventListener('click', () => {
-    addBook('title3', 'author3');
-    displayBooks();
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  addBook(title.value, author.value);
+  displayBooks();
 });
