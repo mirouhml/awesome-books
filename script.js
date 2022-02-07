bba42348a0f134104d398993ee0eb1e0699fac14
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-02-07 14:25:06 +0100

    Create function displayBooks to display in html.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let books = []
+let books = [
+    {
+        title: 'book1',
+        author: 'author1'
+    },
+    {
+        title: 'book2',
+        author: 'author2'
+    },
+]
 
 function addBook(title,author) {
     const book = {
@@ -8,6 +17,20 @@ function addBook(title,author) {
     books.push(book);
 }
 
-function removeBook(title,author) {
+function removeBook(title) {
     books = books.filter(book => book.title !== title)
 }
+
+function displayBooks() {
+    const booksContainer = document.getElementById('books-list');
+    for(let i=0; i<books.length; i+=1) {
+        const book = document.createElement('li');
+        book.innerHTML = `<h2>${books[i].title}</h2>
+                        <h3>${books[i].author}</h3>
+                        <button type="button">Remove</button>
+                        <hr>`;
+        booksContainer.appendChild(book);
+    }
+}
+
+displayBooks();
